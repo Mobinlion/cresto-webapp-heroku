f7e49ec4301e6a4c8b5a8992524f4405743b1ae7
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2018-12-21 15:46:27 +0800

    ok

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,6 @@ import Layout from "../components/Layout";
 
 export default () => (
   <Layout>
-    <p>This is About page, I will mak eit beautiful soon</p>
+    <p>This is About page, I will make it beautiful soon</p>
   </Layout>
 );
