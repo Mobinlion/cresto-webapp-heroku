8302eebfa075e033ecaf76e1b6aa084727e3d290
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2018-12-21 15:35:07 +0800

    ok

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Cresto web app
 
-https://cresto-10-fqdabkikr.now.sh
+Hello world
--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,6 @@ import Layout from "../components/Layout";
 
 export default () => (
   <Layout>
-    <p>This is About page</p>
+    <p>This is About page, I will mak eit beautiful soon</p>
   </Layout>
 );
