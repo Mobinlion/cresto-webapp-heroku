94b944436e56cf690e9f7db55a955d4f4d5a5156
Akhmadjon Ruzikulov <aruzikulov@hust.edu.cn>
2018-12-22 19:11:47 +0800

    ok

--- site-manifest.js
+++ site-manifest.js
@@ -2,5 +2,5 @@ export const links = {
   spectrum: 'https://spectrum.chat/cresto-io',
   submitShowcase:
     'https://spectrum.chat/thread/e425a8b6-c9cb-4cd1-90bb-740fb3bd7541',
-  license: 'https://github.com/aruzikulov/cresto-webapp/license.md'
+  license: 'https://github.com/aruzikulov/cresto-webapp/blob/master/license.md'
 };
